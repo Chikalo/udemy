d1e63d2796c371772b1f0c8f2f9d3e4ab2d9d698
Vladimirm <vladimir.chikalo@gmail.com>
2020-07-28 00:39:38 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer))
+
+const category = "tits";
+
+console.log(`https://url.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`)
